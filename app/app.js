437bce13ecaf4f9c03003f5e214a2cf0da138e40
JeongJaeHa <jaeha2324@gmail.com>
2022-07-09 13:23:53 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const app = express();
+const home = require("./src/routes/home")
+
+
+app.set("views", "./src/views"); //화면 뷰가 관리되는 폴더 
+app.set("view engine", "ejs"); //뷰 엔진이 어떤모듈로 실행될 것인지 설정
+
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+
+module.exports = app;
