838c218e923524c5dbd99b4b80cccb8b398436d0
JeongJaeHa <jaeha2324@gmail.com>
2022-07-11 18:35:08 +0900

    로그인 인증기능 만들기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 const output = {
   home : (req, res) => {
     res.render("home/index"); //경로 설정
@@ -10,10 +12,29 @@ const output = {
   },
 };
 
+const users = {
+  id: ["박대리", "김주임", "김과장"],
+  pw: ["aaaa", "bbbb", "cccc"]
+};
 
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+    pw = req.body.pw;
+
+    if(users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
