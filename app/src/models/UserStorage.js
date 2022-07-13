bb29379c84984257815e2724ba78818510d8c9cb
JeongJaeHa <jaeha2324@gmail.com>
2022-07-13 15:36:54 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,7 +17,18 @@ class UserStorage {
     }, {});
     return newUsers;
   }
+    static getUserInfo(id) {
+      const users = this.#users;
+      const idx = users.id.indexOf(id); 
+      const userKeys = Object.keys(users);
+      const userInfo = Object.keys(users).reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+      }, {});
+      return userInfo;;
+    }
+  }
 
-}
+                                                                                                                                                                                                                                                                                                                                                                                                                      
 
-module.exports = UserStorage;
+module.exports = UserStorage;                  
